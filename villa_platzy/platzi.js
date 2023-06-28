43bfc8c060485b3ba1305e6676c304ad69f74e8f
Angel Quintero <quintero9@gmail.com>
2023-06-27 22:27:55 -0400

    Se actualiza para agregar el campo y el animal

--- villa_platzy/platzi.js
+++ villa_platzy/platzi.js
@@ -1,3 +1,44 @@
+var campo = document.getElementById("campovillaplatzi")
+var lienzo = campo.getContext("2d");
+var mapa = "img/tile.png";
+
+var fondo = new Image();
+fondo.src = "img/tile.png";
+fondo.addEventListener("load", dibujoCampos);
+
+var vacas = new Image();
+vacas.src = "img/vaca.png";
+vacas.addEventListener("load", dibujoVacas);
+
+
+function dibujoCampos() {
+    lienzo.drawImage(fondo, 0, 0);
+
+}
+
+function dibujoVacas() {
+    
+    for(var i=0; i < 10; i++){
+        lienzo.drawImage(vacas, 100, 100);
+    }
+
+}
+
+
+
+
+
+
+// y esta es la funcion que ejecuta el numero aleatorio.
+function numeros(mini, maxi) {
+    var aleatorio;
+    aleatorio = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
+    return aleatorio;
+}
+
+
+
+
 
 
 
