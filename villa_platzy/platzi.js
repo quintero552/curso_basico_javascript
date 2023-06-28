147bd052db9d5bab02d4cc1ba63326678a3c6424
Angel Quintero <quintero9@gmail.com>
2023-06-27 21:58:24 -0400

    Este Es el curso Original de Basico Javascript!

---
+++ villa_platzy/platzi.js
@@ -0,0 +1,14 @@
+var z; // se declara una variable
+
+// se realiza un for que indica que va a sumar i que va desde que i sea menr a 10
+for (var i=0; i < 10; i++) {
+    z = numeros(10, 20);
+    document.write( z + " ,");    
+}
+
+// y esta es la funcion que ejecuta el numero aleatorio.
+function numeros(mini, maxi) {
+    var aleatorio;
+    aleatorio = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
+    return aleatorio;
+}
